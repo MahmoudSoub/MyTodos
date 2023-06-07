3f9f011c8e6bffabd5c07b093a70231029f4eb26
Mahmoud Soub <mahmoudsoubb@gmail.com>
2023-06-07 04:53:05 +0300

    added completed state to Local Storage

--- app.js
+++ app.js
@@ -1,75 +1,104 @@
 const addForm = document.querySelector(".add");
 const search = document.querySelector(".search input");
 const list = document.querySelector(".todos");
-const getTodos = () => JSON.parse(localStorage.getItem("todos")) || [];
 
-const generateTemplate = todo => `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-      <span>${todo}</span>
+// Retrieve todos from local storage
+const getTodosFromLS = () => JSON.parse(localStorage.getItem("todos")) || [];
+
+// Render todos from local storage
+const renderTodos = () => {
+  const todos = getTodosFromLS();
+  todos.forEach(todo => {
+    const template = generateTemplate(todo);
+    list.insertAdjacentHTML("beforeend", template);
+  });
+};
+
+// Generate HTML template for each todo item
+const generateTemplate = todo => {
+  const completedClass = todo.completed ? "completed" : "";
+  return `
+    <li class="list-group-item d-flex justify-content-between align-items-center ${completedClass}">
+      <span>${todo.todo}</span>
       <div class="icons">
         <i class="fa-solid fa-check"></i>
         <i class="far fa-trash-alt delete"></i>
       </div>
     </li>
-`;
-
-const renderLocalStorageTodos = () => {
-  const todos = getTodos();
-  todos.forEach(todo => {
-    const generatedTemplate = generateTemplate(todo);
-    list.innerHTML += generatedTemplate;
-  });
+  `;
 };
-renderLocalStorageTodos();
 
-// add todos event
+// Add todo
 addForm.addEventListener("submit", e => {
   e.preventDefault();
-  const todo = addForm.add.value.trim();
+  const todoInput = addForm.add.value.trim();
 
-  if (todo.length > 0) {
-    const todos = getTodos();
-    localStorage.setItem("todos", JSON.stringify([...todos, todo]));
-    const generatedTemplate = generateTemplate(todo);
-    list.innerHTML += generatedTemplate;
+  if (todoInput.length > 0) {
+    const todos = getTodosFromLS();
+    const newTodo = { todo: todoInput, completed: false };
+    todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    const template = generateTemplate(newTodo);
+    list.insertAdjacentHTML("beforeend", template);
     addForm.reset();
   }
 });
 
-// delete todos event
+// Delete todo
 list.addEventListener("click", e => {
   if (e.target.classList.contains("delete")) {
-    const deletedTodo = e.target.parentElement.parentElement;
+    const deletedTodo = e.target.closest("li");
     deletedTodo.remove();
-    const todos = getTodos();
-    const deletedTodoText = deletedTodo.textContent.trim();
-    const filteredTodos = todos.filter(todo => todo.trim() != deletedTodoText);
+
+    const todos = getTodosFromLS();
+    const deletedTodoText = deletedTodo
+      .querySelector("span")
+      .textContent.trim();
+    const filteredTodos = todos.filter(
+      todo => todo.todo.trim() !== deletedTodoText
+    );
     localStorage.setItem("todos", JSON.stringify(filteredTodos));
   }
 });
 
-// completed
+// Toggle completed state
 list.addEventListener("click", e => {
   if (e.target.classList.contains("fa-check")) {
-    const completedTodo = e.target.parentElement.parentElement;
+    const completedTodo = e.target.closest("li");
     completedTodo.classList.toggle("completed");
+
+    const todos = getTodosFromLS();
+    const completedTodoText = completedTodo
+      .querySelector("span")
+      .textContent.trim();
+    const updatedTodos = todos.map(todo => {
+      if (todo.todo.trim() === completedTodoText) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
   }
 });
 
-// filter todos event
+// Filter todos
 const filterTodos = term => {
-  // add filtered class
-  Array.from(list.children)
-    .filter(todo => !todo.textContent.toLowerCase().includes(term))
-    .forEach(todo => todo.classList.add("filtered"));
-
-  // remove filtered class
-  Array.from(list.children)
-    .filter(todo => todo.textContent.toLowerCase().includes(term))
-    .forEach(todo => todo.classList.remove("filtered"));
+  const todos = Array.from(list.children);
+  todos.forEach(todo => {
+    const todoText = todo.querySelector("span").textContent.toLowerCase();
+    if (todoText.includes(term.toLowerCase())) {
+      todo.classList.remove("filtered");
+    } else {
+      todo.classList.add("filtered");
+    }
+  });
 };
 
 search.addEventListener("keyup", () => {
-  const term = search.value.trim().toLowerCase();
+  const term = search.value.trim();
   filterTodos(term);
 });
+
+// Initial rendering of todos
+renderTodos();
